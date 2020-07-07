6387fc64c140b0fe8d0d160f47be6089b7f0a4b7
Lucas <43614096+Twistere@users.noreply.github.com>
2020-07-07 22:42:29 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using static WindTurbine;
+
+namespace Enedis
+{
+    public class Program
+    {
+        static WindTurbine windTurbine = new WindTurbine();
+        public static async Task Main(string[] args)
+        {
+            string power = await windTurbine.GetPower();
+            Console.WriteLine($"La puissance total est de : {power}");
+        }
+    }
+}
