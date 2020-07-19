711a588dc204852d5ab18cb1e6ea67a590e56285
Lucas <43614096+Twistere@users.noreply.github.com>
2020-07-19 14:45:48 +0200

    update: update main file

--- Program.cs
+++ Program.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Threading.Tasks;
 using static WindTurbine;
+using static SolarPanel;
 
 namespace Enedis
 {
     public class Program
     {
         static WindTurbine windTurbine = new WindTurbine();
+        static SolarPanel solarPanel = new SolarPanel();
         public static async Task Main(string[] args)
         {
             string power = await windTurbine.GetPower();
-            Console.WriteLine($"La puissance total est de : {power}");
+            Console.WriteLine($"La puissance total des éoliens est de : {power}");
+
+            string power2 = await solarPanel.getPower();
+            Console.WriteLine($"La puissance total des panneaux solaires est de : {power2}");
         }
     }
 }
